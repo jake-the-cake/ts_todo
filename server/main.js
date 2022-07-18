ee221f8bc65cca9c1056f966c7a0fc963ed3fee5
jake-the-cake <askjake331@gmail.com>
2022-07-18 15:55:05 -0400

    install react frontend and express backend

---
+++ server/main.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get('/', (req,res) => {
+	console.log('hey, jake')
+	res.status(200).send('hey, jake, but on the screen')
+})
+
+app.listen(process.env.PORT || 3000, () => {
+	console.log('server up on 3k')
+})
