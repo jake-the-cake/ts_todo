991c937550d07f91f69fbbae3886cd4949223a6e
jake-the-cake <askjake331@gmail.com>
2022-07-19 10:27:32 -0400

    begin designing layout

---
+++ client/src/pages/DisplayList.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react'
+import '../static/list_styles.css'
+
+export const DisplayList: FC = () => {
+	return (
+		<div className='list__container'>
+			<div className="list__item">
+				<div className="list__item--checkbox">
+					<input type="checkbox" className='checkbox completed' name="is-complete" id="a" />
+				</div>
+				<div className="list__item--title">Do a thing</div>
+				<div className="list__item--options">...</div>
+			</div>
+			<div className="list__item">
+				<div className="list__item--checkbox">
+					<input type="checkbox" className='checkbox' name="is-complete" id="a" />
+				</div>
+				<div className="list__item--title">Do a thing</div>
+				<div className="list__item--options">...</div>
+			</div>
+		</div>
+	)
+}
