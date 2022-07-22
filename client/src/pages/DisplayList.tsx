2bfe7d495c4174c27b30e47ddf14a166fdffcbaa
jake-the-cake <askjake331@gmail.com>
2022-07-22 13:17:49 -0400

    add check/uncheck on click

--- client/src/pages/DisplayList.tsx
+++ client/src/pages/DisplayList.tsx
@@ -1,19 +1,27 @@
 import React, { FC } from 'react'
 import '../static/list_styles.css'
 
+setTimeout(() => {
+	console.log('hi')
+},3000)
+
 export const DisplayList: FC = () => {
+	const handleClick = () => {
+		document.getElementById('a')?.classList.toggle('completed')
+	}
+
 	return (
 		<div className='list__container'>
-			<div className="list__item">
+			<div className="list__item completed" id='a' onClick={handleClick}>
 				<div className="list__item--checkbox">
-					<input type="checkbox" name="is-complete" id="a" />
+					<input type="checkbox" className='checkbox' name="is-complete" />
 				</div>
 				<div className="list__item--title">Do a thing</div>
 				<div className="list__item--options">...</div>
 			</div>
 			<div className="list__item">
 				<div className="list__item--checkbox">
-					<input type="checkbox" className='checkbox' name="is-complete" id="a" />
+					<input type="checkbox" className='checkbox' name="is-complete" />
 				</div>
 				<div className="list__item--title">Do a thing</div>
 				<div className="list__item--options">...</div>
